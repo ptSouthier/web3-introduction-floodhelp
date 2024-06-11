faf389698cb30daf07dcd6ccd4b644d0acc954bf
ptSouthier <patrick.southier@hotmail.com>
2024-06-11 13:15:22 -0300

    feat: implementing doLogin function at Web3Services file

---
+++ dapp/src/services/Web3Services.ts
@@ -0,0 +1,13 @@
+import Web3 from "web3";
+
+export async function doLogin() {
+  if (!window.ethereum) throw new Error('MetaMask is not installed!');
+
+  const web3 = new Web3(window.ethereum);
+  const accounts = await web3.eth.requestAccounts();
+
+  if (!accounts || !accounts.length) throw new Error('Wallet not found!');
+
+  localStorage.setItem('wallet', accounts[0].toLowerCase());
+  return accounts[0];
+};
