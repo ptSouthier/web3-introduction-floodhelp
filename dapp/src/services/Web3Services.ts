2469f3e89b4e43aca26482d7af31d3f81a5fe6bc
ptSouthier <patrick.southier@hotmail.com>
2024-06-11 20:47:13 -0300

    feat: implementing getOpenHelpRequests function at Web3Services file

--- dapp/src/services/Web3Services.ts
+++ dapp/src/services/Web3Services.ts
@@ -1,4 +1,8 @@
 import Web3 from "web3";
+import ABI from "./ABI.json";
+import { HelpRequest } from "@/types";
+
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 
 export async function doLogin() {
   if (!window.ethereum) throw new Error('MetaMask is not installed!');
@@ -10,4 +14,17 @@ export async function doLogin() {
 
   localStorage.setItem('wallet', accounts[0].toLowerCase());
   return accounts[0];
+};
+
+export async function getOpenHelpRequests(lastId = 0): Promise<HelpRequest[]> {
+  if (!window.ethereum) throw new Error('MetaMask is not installed!');
+
+  const loggedWallet = localStorage.getItem('wallet') || undefined;
+  const web3 = new Web3(window.ethereum);
+
+  const contract = new web3.eth.Contract(ABI, CONTRACT_ADDRESS, { from: loggedWallet });
+  const helpRequests: HelpRequest[] = await contract.methods.getOpenHelpRequests(lastId + 1).call();
+  const validHelpRequests = helpRequests.filter((helpRequest) => helpRequest.title !== '');
+
+  return validHelpRequests;
 };
